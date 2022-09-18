d9af287da87c8e4377bf348f007bfc8d01ec2333
unicornftk <laiyunghwei@gmail.com>
2022-09-19 00:21:11 +0800

    Create sql.h

---
+++ 成品/tcpserver/sql.h
@@ -0,0 +1,20 @@
+#ifndef SQL_H
+#define SQL_H
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlError>
+#include <QtSql/QSqlQuery>
+#include <QDebug>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QLabel>
+
+class Sql
+{
+public:
+    Sql();
+private slots:
+
+};
+
+
+#endif // SQL_H
